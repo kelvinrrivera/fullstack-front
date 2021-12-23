41de6968280529c82f0aca29ec49b2fa7a612906
kelvinrrivera <kelvinrrivera@gmail.com>
2021-12-23 23:15:46 +0100

    New funtion update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,40 @@
-$(document).ready(function(){
-
-    $("#form1").submit(function () {  
-        
-        if($("#ifname, #ilname").val().length < 1) {  
-            $("#fname, #lname").css("color", "red");
-            event.preventDefault();
-        }
-    });
-
-});
-
-$("#tamano").on('change', function() {
- console.log(this.value);
- // Aqui va la llamada AJAX con JQuery
-    
-    $.ajax({
-        type: "POST",
-        url: "/checksize",
-        success: function (result) {
-        $("tamaño").text(result);
-        }
-        });
-
-    
-});
+var form1 = $("#form1");
+   form1.on("submit", function(e) { 
+       
+       let nombre  = $("#ifname").val();
+       let apellido = $("#ilname").val();
+       
+       if(nombre=="")
+       {
+           setColor('fname','red');
+           e.preventDefault();        
+       }
+   
+       if(apellido=="")
+       {
+           setColor('lname','red');
+           e.preventDefault();        
+       }
+   });
+   
+   
+   $("#tamano").on('change', function() {
+   
+       // Aqui va la llamada AJAX con JQuery
+       let form1 = new FormData();
+       form1.append("tamano", this.value);
+   
+       let settings = {
+         "url": "http://localhost:5000/checksize",
+         "method": "POST",
+         "timeout": 0,
+         "processData": false,
+         "mimeType": "multipart/form-data",
+         "contentType": false,
+         "data": form1
+       };
+   
+       $.ajax(settings).done(function (response) {	  
+         $("#resultado_tamano").text(response);
+       });
+   });
